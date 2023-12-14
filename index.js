14204704b19c08908bf1afb0e905c5ad7113a886
Akanimo <Akanumoh4@gmail.com>
2023-12-14 01:51:49 +0100

    Final Project Prototype

---
+++ index.js
@@ -0,0 +1,19 @@
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  // document.querySelector(".togglenav.active").style.top = "0";
+  prevScrollpos = currentScrollPos;
+}
+
+function toggleMenu() {
+  const menuToggle = document.querySelector('.menuToggle');
+  const secondary_nav = document.querySelector('.secondary-nav');
+  menuToggle.classList.toggle('active')
+  secondary_nav.classList.toggle('active')
+  // document.querySelector(.)
+}
